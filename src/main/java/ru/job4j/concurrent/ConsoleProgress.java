d758c022fe1520f5b847dcac0df2030f3e12e25b
redgad88 <vadim_kuzin@list.ru>
2023-11-20 15:50:25 +0300

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -20,7 +20,7 @@ public class ConsoleProgress implements Runnable {
                     Thread.sleep(500);
                 }
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                Thread.currentThread().interrupt();
             }
         }
     }
