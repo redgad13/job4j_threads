b4c521e2005fc57f42580be7e19ed5e430051930
redgad88 <vadim_kuzin@list.ru>
2023-11-20 17:06:33 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,49 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int i = 0; i < 101; i++) {
-                        System.out.print("\rLoading : " + i + "%");
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-        );
-        thread.start();
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var startAt = System.currentTimeMillis();
+        try (var in = new URL(url).openStream();
+             var out = new FileOutputStream("tmp")) {
+            var elapsedTime = System.currentTimeMillis() - startAt;
+            if (elapsedTime < speed) {
+                Thread.sleep(speed - elapsedTime);
+            }
+            var dataBuffer = new byte[512];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                out.write(dataBuffer, 0, bytesRead);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (!args[0].startsWith("http")) {
+            throw new IllegalArgumentException("введите ссылку, начиная с http");
+        }
+        if (Integer.parseInt(args[1]) < 0) {
+            throw new IllegalArgumentException("значение должно быть больше нуля");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
