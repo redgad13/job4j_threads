3363677f3ce8bd390fa223dbdbc518b099d39cbf
redgad88 <vadim_kuzin@list.ru>
2023-11-22 17:12:30 +0300

    9. ThreadLocal [#504912 #461809]

---
+++ src/main/java/ru/job4j/concurrent/threads/threadlocal/SecondThread.java
@@ -0,0 +1,10 @@
+package ru.job4j.concurrent.threads.threadlocal;
+
+public class SecondThread extends Thread {
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 2.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+
+}
