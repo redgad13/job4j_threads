1fdeb1446503ce795eab4d7511a14cb49fd87836
redgad88 <vadim_kuzin@list.ru>
2023-11-22 13:07:08 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,6 +3,8 @@ package ru.job4j.concurrent;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Wget implements Runnable {
     private final String url;
@@ -15,29 +17,37 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        var startAt = System.currentTimeMillis();
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream("tmp")) {
-            var elapsedTime = System.currentTimeMillis() - startAt;
-            if (elapsedTime < speed) {
-                Thread.sleep(speed - elapsedTime);
-            }
-            var dataBuffer = new byte[512];
+            var dataBuffer = new byte[1024];
             int bytesRead;
+            long elapsedTime;
+            var startAt = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
                 out.write(dataBuffer, 0, bytesRead);
+                elapsedTime = startAt - System.currentTimeMillis();
+                var currentSpeed = (1024 / elapsedTime) * 1_000_000;
+                if (speed < currentSpeed) {
+                    Thread.sleep(currentSpeed - speed);
+                }
             }
-
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private static boolean checkUrl(String url) {
+        String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(url);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        if (!args[0].startsWith("http")) {
+        if (!checkUrl(args[0])) {
             throw new IllegalArgumentException("введите ссылку, начиная с http");
         }
-        if (Integer.parseInt(args[1]) < 0) {
+        if (Integer.parseInt(args[1]) <= 0) {
             throw new IllegalArgumentException("значение должно быть больше нуля");
         }
         String url = args[0];
