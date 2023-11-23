72071a1958bbc9c8d478d5057db6db048b084631
redgad88 <vadim_kuzin@list.ru>
2023-11-23 13:51:07 +0300

    8. Скачивание файла с ограничением. [#144271 #461755]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,28 +8,35 @@ import java.util.regex.Pattern;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String destFolder;
     private final int speed;
+    private static final String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+    private static final Pattern pattern = Pattern.compile(regex);
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String destFolder, int speed) {
         this.url = url;
+        this.destFolder = destFolder;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         try (var in = new URL(url).openStream();
-             var out = new FileOutputStream("tmp")) {
+             var out = new FileOutputStream(destFolder)) {
             var dataBuffer = new byte[1024];
             int bytesRead;
-            long elapsedTime;
-            var startAt = System.currentTimeMillis();
+            long totalDownloaded = 0;
+            var startTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
                 out.write(dataBuffer, 0, bytesRead);
-                elapsedTime = startAt - System.currentTimeMillis();
-                var currentSpeed = (1024 / elapsedTime) * 1_000_000;
-                if (speed < currentSpeed) {
-                    Thread.sleep(currentSpeed - speed);
+                totalDownloaded += bytesRead;
+                if (totalDownloaded >= speed) {
+                    var elapsedTime = System.currentTimeMillis() - startTime;
+                    if (elapsedTime < 1000) {
+                        Thread.sleep(1000 - elapsedTime);
+                    }
                 }
+
             }
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
@@ -37,22 +44,27 @@ public class Wget implements Runnable {
     }
 
     private static boolean checkUrl(String url) {
-        String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
         return matcher.matches();
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("проверьте количество введенных аргументов");
+        }
         if (!checkUrl(args[0])) {
             throw new IllegalArgumentException("введите ссылку, начиная с http");
         }
-        if (Integer.parseInt(args[1]) <= 0) {
+        if (args[1].isEmpty()) {
+            throw new IllegalArgumentException("введите имя файла для сохранения");
+        }
+        if (Integer.parseInt(args[2]) <= 0) {
             throw new IllegalArgumentException("значение должно быть больше нуля");
         }
         String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String destFolder = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(url, destFolder, speed));
         wget.start();
         wget.join();
     }
