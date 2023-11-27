63674fe3c9986e4739b01dc696691b8ef9285699
redgad88 <vadim_kuzin@list.ru>
2023-11-27 17:11:16 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/test/java/ru/job4j/CountTest.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CountTest {
+
+    @Test
+    void whenExecute2ThreadThen2() throws InterruptedException {
+        var count = new Count();
+        var first = new Thread(count::increment);
+        var second = new Thread(count::increment);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get()).isEqualTo(2);
+    }
+}
