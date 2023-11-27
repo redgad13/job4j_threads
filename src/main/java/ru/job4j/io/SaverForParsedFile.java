6e0877b7db614afff5fff6e96e2fc50fec8b3a6d
redgad88 <vadim_kuzin@list.ru>
2023-11-27 16:38:14 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/SaverForParsedFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public final class SaverForParsedFile {
+    private final File file;
+
+    public SaverForParsedFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i++) {
+                out.write(content.charAt(i));
+            }
+        }
+    }
+}
