b294d5e2c1b1777d6c20c993901883cfcda8fe3b
redgad88 <vadim_kuzin@list.ru>
2023-12-13 16:13:02 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,32 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        if (memory.get(model.id()).version() != model.version()) {
+            throw new OptimisticException("Версии не одинаковы");
+        }
+        return memory.computeIfPresent(model.id(), (k, v) -> new Base(model.id(), model.name(), model.version() + 1)) != null;
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
