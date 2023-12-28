0a81ea78b60f38b90de4db21aff936fb2e543287
redgad88 <vadim_kuzin@list.ru>
2023-12-28 10:22:16 +0300

    4. CompletableFuture [#361626 #468006]

--- src/main/java/ru/job4j/pools/Sums.java
+++ src/main/java/ru/job4j/pools/Sums.java
@@ -32,8 +32,12 @@ public class Sums {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Sums sums = (Sums) o;
         return rowSum == sums.rowSum && colSum == sums.colSum;
     }
